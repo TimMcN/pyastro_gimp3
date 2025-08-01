15b9a44662c7c5eb5046f5ea0747ecb188b61890
TimMcN <timothyjmcnamara@gmail.com>
2025-08-01 18:24:41 +0100

    Added preview to gaussian blur

--- pyastro_big_gaussian_blur/pyastro_big_gaussian_blur.py
+++ pyastro_big_gaussian_blur/pyastro_big_gaussian_blur.py
@@ -3,6 +3,7 @@ import sys
 import gi
 gi.require_version('Gimp', '3.0')
 from gi.repository import Gimp
+from gi.repository import Gegl
 gi.require_version('GimpUi', '3.0')
 from gi.repository import GimpUi
 from gi.repository import GLib
@@ -37,24 +38,44 @@ class pyastro_big_gaussian_blur(Gimp.PlugIn):
     def run(self, procedure, run_mode, image, drawables, config, run_data):
         if run_mode == Gimp.RunMode.INTERACTIVE:
             GimpUi.init("pyastro_big_gaussian_blur")
-            Gimp.context_push()
+            self.prev = False
+
             dialog=GimpUi.ProcedureDialog.new(procedure=procedure, config=config)
             widget = dialog.get_widget("radius", GimpUi.SpinScale)
-            box = dialog.fill(None)
+            filter = Gimp.DrawableFilter.new(drawables[0], 'gegl:gaussian-blur')
+            image.undo_group_start()
 
+            Gimp.context_push()    
+            def preview(widget):
+                if self.prev:
+                    image.remove_layer(image.get_layers()[-1])
+                self.prev=True
+                self.l1 = Gimp.Layer.copy(drawables[0])
+                filter = Gimp.DrawableFilter.new(self.l1, 'gegl:gaussian-blur')
+                gconf = filter.get_config()
+                gconf.set_property('std-dev-x', widget.get_value())
+                gconf.set_property('std-dev-y', widget.get_value())
+                self.l1.merge_filter(filter)
+                image.insert_layer(self.l1, None, -1)
+                Gimp.displays_flush()
+            widget.connect("value-changed", preview)
+            box = dialog.fill(None)
+            preview(widget)
             if not dialog.run():
+                image.remove_layer(image.get_layers()[-1])
+                Gimp.displays_flush()
                 dialog.destroy()
+                image.undo_group_end()
                 return procedure.new_return_values(Gimp.PDBStatusType.CANCEL, None)
             else:
                 dialog.destroy()
-            image.undo_group_start()
-            filter = Gimp.DrawableFilter.new(drawables[0], 'gegl:gaussian-blur')
-            gconf = filter.get_config()
-            gconf.set_property('std-dev-x', config.get_property("radius"))
-            gconf.set_property('std-dev-y', config.get_property("radius"))
-            drawables[0].merge_filter(filter)
-            image.undo_group_end()
+                image.merge_down(self.l1, 0)
+                image.undo_group_end()
             Gimp.context_pop()
+            
         return procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, None)
+    
+
+
 Gimp.main(pyastro_big_gaussian_blur.__gtype__, sys.argv)
 
